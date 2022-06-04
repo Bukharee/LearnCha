8adb8b45868807f441057ede5499d85a64f82f8a
Bukhari Auwal Idris <idrisbukhariauwal@gmail.com>
2022-06-03 22:20:57 -0700

    feeeded quiz questions to frontend

--- Books/urls.py
+++ Books/urls.py
@@ -1,8 +1,9 @@
 from importlib.resources import path
 from django.urls import path
-from .views import books_list, subjects_list
+from .views import books_list, subjects_list, quiz
 
 urlpatterns = [
     path('subjects/', subjects_list, name="subjects"),
-    path('grade/<slug:subject_title>/', books_list, name="books")
+    path('grade/<slug:subject_title>/', books_list, name="books"),
+    path('quiz/<slug:subject_title>/', quiz, name="quiz"),
 ]
--- Books/views.py
+++ Books/views.py
@@ -1,7 +1,8 @@
 from turtle import title
 from django.shortcuts import render, get_object_or_404
-from .models import Subject, Books
+from .models import Subject, Books, Answer
 from django.contrib.auth.decorators import login_required
+import random
 
 # Create your views here.
 
@@ -25,3 +26,16 @@ def books_list(request, subject_title):
         books = Books.objects.filter(subject=subject, grade=request.user.grade)
         print(books)
         return render(request, 'books_list.html', {"books": books, "message": "Valid Subject"})
+
+
+@login_required
+def quiz(request, subject_title):
+    try:
+        subject = get_object_or_404(Subject, slug=subject_title)
+    except:
+        return render(request, 'quiz.html', {"questions": None, "message": 
+        "not a valid subject"})
+    else:
+        questions = Answer.objects.all()
+        number_of_questions = questions.count()
+        return render(request, 'quiz.html', {"questions": questions})
